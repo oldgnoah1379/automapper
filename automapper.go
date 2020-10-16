93f99c4ff3b80d7a7dfa0d5e57de34f004975a45
hoang <gnoah.deptrai@gmail.com>
2020-10-16 14:31:25 +0700

    append list mapping

--- automapper.go
+++ automapper.go
@@ -12,18 +12,7 @@ type AutoMapper struct {
 	srcTemplateType reflect.Type
 }
 
-func (atm *AutoMapper) Mapping(src interface{}, dst interface{}) error {
-	err := &InvalidMappingError{SrcType: reflect.TypeOf(src), DstType: reflect.TypeOf(dst)}
-	if !CompareType(src, atm.srcTemplateType) ||
-		!CompareType(dst, atm.dstTemplateType) ||
-		!IsPointer(src) ||
-		!IsPointer(dst) ||
-		IsNil(src) ||
-		IsNil(dst) {
-		return err
-	}
-	srcMap := NewFieldMap(src)
-	dstMap := NewFieldMap(dst)
+func (atm AutoMapper) mapping(srcMap, dstMap FieldMap) {
 	ign := reflect.ValueOf(Ignore)
 	for tag, field := range dstMap {
 		handler, ok := atm.transforms[tag]
@@ -39,6 +28,58 @@ func (atm *AutoMapper) Mapping(src interface{}, dst interface{}) error {
 			field.Set(srcValue)
 			continue
 		}
+	}
+}
+
+func (atm AutoMapper) Mapping(src interface{}, dst interface{}) error {
+	action := "Mapping"
+	dstValue := reflect.ValueOf(dst)
+	srcValue := reflect.ValueOf(src)
+
+	err := GetNilError(action, srcValue, dstValue)
+	if err != nil {
+		return err
+	}
+	err = GetNotPointerError(action, srcValue.Type(), dstValue.Type())
+	if err != nil {
+		return err
+	}
+	err = GetDifferentTypeError(action, false, srcValue.Type().Elem(), dstValue.Type().Elem(),
+		atm.srcTemplateType, atm.dstTemplateType)
+	if err != nil {
+		return err
+	}
+	atm.mapping(NewFieldMapFormPointer(src), NewFieldMapFormPointer(dst))
+	return nil
+}
+
+func (atm *AutoMapper) ListMapping(src interface{}, dst interface{}) error {
+	action := "ListMapping"
+	dstValue := reflect.ValueOf(dst)
+	srcValue := reflect.ValueOf(src)
+	err := GetNilError(action, srcValue, dstValue)
+	if err != nil {
+		return err
+	}
+	err = GetNotListError(action, srcValue.Type(), dstValue.Type())
+	if err != nil {
+		return err
+	}
+	err = GetDifferentTypeError(action, true, srcValue.Type().Elem(), dstValue.Type().Elem(),
+		atm.srcTemplateType, atm.dstTemplateType)
+	if err != nil {
+		return err
+	}
+	dstMax := dstValue.Len()
+	srcMax := dstValue.Len()
+	max := dstMax
+	if srcMax < dstMax {
+		max = srcMax
+	}
+	for i := 0; i < max; i++ {
+		srcField := srcValue.Index(i)
+		dstField := dstValue.Index(i)
+		atm.mapping(NewFieldMapFromValue(srcField), NewFieldMapFromValue(dstField))
 	}
 	return nil
 }
